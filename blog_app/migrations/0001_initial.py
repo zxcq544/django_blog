b0a9eb0e014b649b435815fbc89c4207d2674d53
zxcq544 <anisimovsa@gmail.com>
2015-05-30 16:41:37 +0300

    Commit 1

--- blog_app/migrations/0001_initial.py
+++ blog_app/migrations/0001_initial.py
@@ -13,16 +13,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Comment',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
-                ('text', models.CharField(max_length=256)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('comment_text', models.CharField(max_length=256)),
                 ('votes', models.IntegerField(default=0)),
             ],
         ),
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
-                ('text', models.CharField(max_length=500)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('post_title', models.CharField(verbose_name='Заголовок новости', max_length=100)),
+                ('post_text', models.CharField(verbose_name='Текст новости', max_length=500)),
                 ('pub_date', models.DateTimeField(verbose_name='date published')),
             ],
         ),
--- blog_app/tests.py
+++ blog_app/tests.py
@@ -17,3 +17,21 @@ class PostMethodTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_post = Post(pub_date=time)
         self.assertEqual(future_post.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_post(self):
+        """
+        was_published_recently() should return False for posts whose
+        pub_date is older than 1 day.
+         """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_post = Post(pub_date=time)
+        self.assertEqual(old_post.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_post(self):
+        """
+        was_published_recently() should return True for posts whose
+        pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_post = Post(pub_date=time)
+        self.assertEqual(recent_post.was_published_recently(), True)
