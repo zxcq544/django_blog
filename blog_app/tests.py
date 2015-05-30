d46123d838efeeb289b3f64c5b40f9dced594281
zxcq544 <anisimovsa@gmail.com>
2015-05-30 14:26:24 +0300

    Commit 1

--- blog_app/tests.py
+++ blog_app/tests.py
@@ -1,3 +1,19 @@
+# Create your tests here.
+
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+
+class PostMethodTests(TestCase):
+    def test_was_published_recently_with_future_post(self):
+        """
+        was_published_recently() should return False for posts whose
+        pub_date is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_post = Post(pub_date=time)
+        self.assertEqual(future_post.was_published_recently(), False)
